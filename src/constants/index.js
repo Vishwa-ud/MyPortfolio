d80d7899bad4efd0b0d1e43fbe417743e339cf54
Vishwa-ud <it21315282@my.sliit.lk>
2025-12-02 20:20:49 +0530

    Add new images and models; update constants with new word entries
    - Added various image files including logos for Node, Python, React, and Three.js.
    - Introduced new SVG icons for menu and close actions.
    - Included multiple PNG images for projects and other assets.
    - Added new GLB model files for optimized computer and various logos.
    - Updated constants in index.js to include new word entries with associated image paths.

--- src/constants/index.js
+++ src/constants/index.js
@@ -63,6 +63,17 @@ import {
       isRoute: true,
     },
   ];
+
+  const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
   
   const services = [
     {
