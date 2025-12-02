d70e990fe17ccff0903018788de55a9359e9959b
Vishwa-ud <it21315282@my.sliit.lk>
2025-12-03 00:24:33 +0530

    feat: Enhance ProExperience component with motion effects and updated header structure

--- src/components/Pro-Experience.jsx
+++ src/components/Pro-Experience.jsx
@@ -5,6 +5,9 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { expCards } from "../constants";
 import TitleHeader from "../components/TitleHeader";
 import GlowCard from "../components/GlowCard";
+import { textVariant } from "../utils/motion";
+import { styles } from "../styles";
+import { motion } from "framer-motion";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -95,10 +98,14 @@ const ProExperience = () => {
       className="flex-center md:mt-40 mt-20 section-padding xl:px-0"
     >
       <div className="w-full h-full md:px-20 px-5">
-        <TitleHeader
-          title="Professional Work Experience"
-          sub="💼 My Career Overview"
-        />
+        <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          Professional Work Experience.
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          My Career Overview.
+        </h2>
+      </motion.div>
         <div className="mt-32 relative">
           <div className="relative z-50 xl:space-y-32 space-y-10">
             {expCards.map((card) => (
