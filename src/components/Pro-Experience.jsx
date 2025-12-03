243f76bbd4c49b6eac8efa61701a8ebb368f1445
Vishwa-ud <it21315282@my.sliit.lk>
2025-12-03 17:52:11 +0530

    style: Adjust z-index of experience cards for improved layering in ProExperience component

--- src/components/Pro-Experience.jsx
+++ src/components/Pro-Experience.jsx
@@ -107,7 +107,7 @@ const ProExperience = () => {
         </h2>
       </motion.div>
         <div className="mt-32 relative">
-          <div className="relative z-50 xl:space-y-32 space-y-10">
+          <div className="relative z-10 xl:space-y-32 space-y-10">
             {expCards.map((card) => (
               <div key={card.title} className="exp-card-wrapper">
                 <div className="xl:w-2/6">
